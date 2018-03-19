7b3df043a34af969b17e0538e755a97dbe9d30d1
Ahmad <Ahmad.shannak96@hotmail.com>
2018-03-19 17:22:47 +0200

    Adding the Game

---
+++ Cards In Unity/Assets/Scripts/Card.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewCard",menuName ="Card")]
+public class Card : ScriptableObject {
+
+    public new string name;
+    public string disc;
+
+    public Sprite img;
+
+    public int ad;
+    public int hp;
+    public int mana;
+
+	
+}
---
+++ Cards In Unity/Assets/Scripts/Display.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Display : MonoBehaviour {
+
+    public Card card;
+
+    public Image img;
+    public new Text name;
+    public Text disc;
+
+    public Text mana;
+    public Text hp;
+    public Text ad;
+
+    private void Start()
+    {
+        img.sprite = card.img ;
+
+        name.text = card.name ;
+        disc.text = card.disc ;
+
+        mana.text = card.mana + "" ;
+        hp.text = card.hp + "" ;
+        ad.text = card.ad + "" ;
+
+    }
+}
